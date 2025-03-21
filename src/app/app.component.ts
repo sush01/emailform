7d2cff97d0a2cad9fe7a7b221c14c6c70ba4e8ce
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-21 10:06:51 +0200

    basic template form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'emailform';
+  email:string= '';
+
 }
