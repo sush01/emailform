d0584c5da4c35644bbe3edc444a99ff2177054ad
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-23 22:52:25 +0200

    add form control validation errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
